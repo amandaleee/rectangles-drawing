d4bda26a53dc9802af75f62a5eefda04dba8fdf5
amandaleee <amandalee@amandalee.org>
2016-09-15 21:41:49 -0500

    readme and some other stuff

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+  // console.log("loaded");
+  window.addEventListener('click', function() {
+    // console.log("clicked");
+  });
+}
+
+
+//the above was just making sure it worked
