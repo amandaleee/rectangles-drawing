2eb880e166c62689a8202a35585697ec87e28df3
amandaleee <amandalee@amandalee.org>
2016-09-16 00:10:56 -0500

    updated script

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,4 @@
 $(document).ready(function(){
-  // var 
-  // $("canvas").on("mousemove", function(e) {
-  //   var rectangleArray = [];
-  //   if (e.which == 1) {
-  //     $(".xy").text("X: " + e.pageX + " Y: " + e.pageY);
-  //   //TODO: add each of these to a hash
-  //   //push the hash to rectangleArray
-  //     console.log(e.pageX + ", " + e.pageY);
-  //   }
-  //   //TODO: after mouseup, delete everything from rectangleArray except the first and last elements. 
-  // });
   $("canvas").on("mousedown", function(e) {
     $(".xydown").text("X: " + e.pageX + " Y: " + e.pageY);
   });
