f25a03bdecf6276f3083a556f90b316aaf1c4be2
amandaleee <amandalee@amandalee.org>
2016-09-21 15:47:28 -0500

    draggable works

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
+
+  // a reusable rectangle class to append
+  // var count = 1 - for iterating
   var rect = "<div class=\"rectangle\"></div>";
+  
+
   $(".add-rect").click(function(){
-    // console.log("clicked btn");
+    // append it
     $(".canvas").append(rect);
+    //make it draggable - iterate on this and make it target ONLY the rect that i just added, not all of them. 
+    $(".rectangle").draggable();
   });
 });
